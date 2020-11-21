172f8ed16eb0192ea67a527ef56b3dc68b231637
emmiekao <emmiekao@localhost>
2020-11-20 18:27:32 -0800

    hi

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -16,52 +16,68 @@ public class Houses {
 			Random ran=new Random();
 			int x=ran.nextInt(3);
 			if (x==0) {
-			drawHouse("small");
+			drawPointyRoof("small");
 		}
 			else if (x==1) {
-				drawHouse("medium");
+				drawPointyRoof("medium");
 			}
 			else if (x==2){
-				drawHouse("large");
+				drawFlatRoof();
 			}
 		}
+		
 	}
-	static void drawFlatRoof(String height) {
-		int y=0;
-		if(height.equals("small")) {
-		y=60;
-		}
-else if(height.equals("medium")) {
-			y=125;
-		}
-else if (height.equals("large")){
-	y=200;
-	}
-	
+	static void drawFlatRoof() {
+
+
 		robot.penDown();
+		robot.setPenWidth(20);
 		robot.setPenColor(32, 108, 58);
 		robot.move(25);
 		robot.setRandomPenColor();
 		robot.turn(90);
-		robot.move(y);
+		robot.move(200);
 		
 		
-		robot.move(y);
+		robot.turn(-90);
+		robot.move(50);
+		robot.turn(-90);
+		
+		
+		robot.move(200);
 		robot.turn(90);
 		robot.setPenColor(32, 108, 58);
 		robot.move(25);}
 		
 	
-static void drawPointyRoof(){
-	robot.turn(-45);
-	robot.move(50);
-	robot.turn(-90);
-	robot.move(50);
-	robot.turn(-45);
+static void drawPointyRoof(String height){
+	
+	int y=0;
+
+if(height.equals("small")) {
+y=60;
+}
+else if(height.equals("medium")) {
+	y=125;
 }
 
-static void drawFlatRoof() {
-	robot.turn(-90);
+
+	robot.penDown();
+	robot.setPenWidth(20);
+	robot.setPenColor(32, 108, 58);
+	robot.move(25);
+	robot.setRandomPenColor();
+	robot.turn(90);
+	robot.move(y);
+	robot.turn(-45);
 	robot.move(50);
 	robot.turn(-90);
-}}
+	robot.move(50);
+	robot.turn(-45);
+	robot.move(y);
+	robot.turn(90);
+	robot.setPenColor(32, 108, 58);
+	robot.move(25);}
+}
+
+
--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -20,41 +20,89 @@ public class PhotoQuiz {
 
 		JFrame quizWindow = new JFrame();
 		quizWindow.setVisible(true);
+		int score=0;
 		// This will make sure the program exits when you close the window
 		quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-
+String pikachuURL="https://i.pinimg.com/originals/55/c6/1c/55c61c14d578dc91e1b11fb5d93e53a6.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+Component component;
 		// 3. use the "createImage()" method below to initialize your Component
-
+component=createImage(pikachuURL);
 		// 4. add the image to the quiz window
-
+quizWindow.add(component);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String answer1=JOptionPane.showInputDialog(null, "What Pokemon is this?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if(answer1.equalsIgnoreCase("pikachu")) {
+	JOptionPane.showMessageDialog(null, "CORRECT");
+	score++;
+}
 		// 8. print "INCORRECT" if the answer is wrong
-
+else {
+	JOptionPane.showMessageDialog(null, "INCORRECT");
+}
+// 6. ask a question that relates to the image
+String answer2=JOptionPane.showInputDialog(null, "What does it evolve from?");
+// 7. print "CORRECT" if the user gave the right answer
+if(answer2.equalsIgnoreCase("pichu")) {
+JOptionPane.showMessageDialog(null, "CORRECT");
+score++;
+}
+// 8. print "INCORRECT" if the answer is wrong
+else {
+JOptionPane.showMessageDialog(null, "INCORRECT");
+}
+// 6. ask a question that relates to the image
+String answer3=JOptionPane.showInputDialog(null, "What does it evolve into?");
+// 7. print "CORRECT" if the user gave the right answer
+if(answer3.equalsIgnoreCase("raichu")) {
+JOptionPane.showMessageDialog(null, "CORRECT");
+score++;
+}
+// 8. print "INCORRECT" if the answer is wrong
+else {
+JOptionPane.showMessageDialog(null, "INCORRECT");
+}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+quizWindow.remove(component);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+String articunojustcuz="https://cdn.bulbagarden.net/upload/thumb/4/4e/144Articuno.png/1200px-144Articuno.png";
 		// 11. add the second image to the quiz window
-
+component=createImage(articunojustcuz);
+quizWindow.add(component);
 		// 12. pack the quiz window
-
+quizWindow.pack();
 		// 13. ask another question
-
+String answer4=JOptionPane.showInputDialog(null, "What Pokemon is this?");
+// 7. print "CORRECT" if the user gave the right answer
+if(answer4.equalsIgnoreCase("articuno")) {
+JOptionPane.showMessageDialog(null, "CORRECT");
+score++;
+}
+// 8. print "INCORRECT" if the answer is wrong
+else {
+JOptionPane.showMessageDialog(null, "INCORRECT");
+}
+String answer5=JOptionPane.showInputDialog(null, "Is it normal, mega, or legendary?");
+// 7. print "CORRECT" if the user gave the right answer
+if(answer5.equalsIgnoreCase("legendary")) {
+JOptionPane.showMessageDialog(null, "CORRECT");
+score++;
+}
+// 8. print "INCORRECT" if the answer is wrong
+else {
+JOptionPane.showMessageDialog(null, "INCORRECT");
+}
 		// 14+ check answer, say if correct or incorrect, etc.
-
+JOptionPane.showMessageDialog(component, "Your total score is " + score + "! Thanks for playing!");
 	}
 
 	private Component createImage(String imageUrl) throws MalformedURLException {
