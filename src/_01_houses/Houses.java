e7e6091256c3e643fb49b8372c9e1f0fcbf8ab04
emmiekao <emmiekao@localhost>
2020-11-20 17:30:11 -0800

    test

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -14,34 +14,54 @@ public class Houses {
 		
 		for (int i = 0; i < 10; i++) {
 			Random ran=new Random();
-			int x=ran.nextInt(250);
-			if (x<=60) {
-			drawHouse(small);
+			int x=ran.nextInt(3);
+			if (x==0) {
+			drawHouse("small");
 		}
-			else if (i<=125) {
-				drawHouse(medium);
+			else if (x==1) {
+				drawHouse("medium");
 			}
-			else {
-				drawHouse(large);
+			else if (x==2){
+				drawHouse("large");
 			}
 		}
 	}
-	static void drawHouse(String height) {
+	static void drawFlatRoof(String height) {
+		int y=0;
 		if(height.equals("small")) {
-			
-		
+		y=60;
+		}
+else if(height.equals("medium")) {
+			y=125;
+		}
+else if (height.equals("large")){
+	y=200;
+	}
+	
 		robot.penDown();
 		robot.setPenColor(32, 108, 58);
 		robot.move(25);
-		robot.setPenColor(0, 0, 0);
+		robot.setRandomPenColor();
 		robot.turn(90);
-		robot.move(height);
-		robot.turn(-90);
-		robot.move(50);
-		robot.turn(-90);
-		robot.move(height);
+		robot.move(y);
+		
+		
+		robot.move(y);
 		robot.turn(90);
 		robot.setPenColor(32, 108, 58);
 		robot.move(25);}
-	}
+		
+	
+static void drawPointyRoof(){
+	robot.turn(-45);
+	robot.move(50);
+	robot.turn(-90);
+	robot.move(50);
+	robot.turn(-45);
 }
+
+static void drawFlatRoof() {
+	robot.turn(-90);
+	robot.move(50);
+	robot.turn(-90);
+}}
